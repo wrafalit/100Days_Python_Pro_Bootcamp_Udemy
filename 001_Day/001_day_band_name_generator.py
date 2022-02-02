d87a1347fbaa36cb4559c3433d7f8abeec969530
wrafalit@gmail.com <Programowanie2020#>
2022-02-02 19:05:41 +0100

    001 Day complete

---
+++ 001_Day/001_day_band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+user_city = input("What's your name of the city you grew up in?\n")
+print(user_city)
+user_pet = input("What's your pet's name?\n")
+print(user_pet)
+print("Your band name could be " + user_city + " " + user_pet)
---
+++ 001_Day/001_exercise.py
@@ -0,0 +1,22 @@
+print('Hallo World')
+print('What to print')
+
+print('Hello world!\n'
+      'Hello world\n'
+      'Hello world!\n'
+      'Hello world')
+
+print("Hello" + " " + "Angela")
+
+# input("What is your name?")
+# print("Hello" + input("what is your name? "))
+
+print(len(input("What is your name?: ")))
+
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a= " +a)
+print("b= " +b)
