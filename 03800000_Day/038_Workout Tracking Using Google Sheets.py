f80e9aecfcd1095bbf3ccf0de2bbcf280a8a50a4
wrafalit <wrafalit@gmail.com>
2022-03-30 22:33:36 +0200

    DAY 38 completed

---
+++ 03800000_Day/038_Workout Tracking Using Google Sheets.py
@@ -0,0 +1,53 @@
+import requests
+from datetime import datetime
+
+# nutritionix
+s://api.sheety.co/2afb7b292d28c56422bb2f510d24b79f/workoutTracking/workouts"
+
+def post_exercise(active):
+
+    user_param = {
+         "query": active,
+         "gender":"male",
+         "weight_kg":84,
+         "height_cm":180,
+         "age":35
+        }
+
+    headers = {
+        "x-app-id" : APP_ID,
+        "x-app-key" : API_KEY,
+        "Content-Type" : "application/json",
+    }
+
+    response = requests.post(url=exercise_end_url, json=user_param, headers=headers)
+    exercise = []
+    action = response.json()["exercises"][0]['user_input']
+    duration = response.json()["exercises"][0]['duration_min']
+    calories = response.json()["exercises"][0]['nf_calories']
+    print(f"Your action: {action}, duration: {duration} and burn {calories} calories.")
+    exercise.append(action)
+    exercise.append(duration)
+    exercise.append(calories)
+    return exercise
+
+def sheety(exercise:list):
+
+    data = {
+        "workout" : {
+                "date": datetime.now().strftime("%d/%m/%Y"),
+                "time": datetime.now().strftime("%H:%M:%S"),
+                "exercise": exercise[0],
+                "duration": exercise[1],
+                "calories": exercise[2]
+            }
+        }
+
+    sheety_post = requests.post(url=end_url, json=data, headers=head)
+
+
+
+# active = input("Tell me which exercise you did? ")
+active =  "runing 30 min"
+exercise = post_exercise(active)
+sheety(exercise)
